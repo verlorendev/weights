b2ecaa209c2ffaba3dd52d071e8a32f9ca8b8e13
Your Name Here <verloren@ukr.net>
2014-10-24 23:34:17 +0300

    continued develepment

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,12 @@ var weightsApp = angular.module('weightsApp', []);
 weightsApp.controller('weightsController', function($scope) {
   $scope.weightsList = [
     {'date': 1394496929115,
+     'weight': 83},
+      {'date': 1394496929115,
+     'weight': 83},
+      {'date': 1394496929115,
+     'weight': 83},
+      {'date': 1394496929115,
      'weight': 83}
   ];
 });
